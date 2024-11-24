8f4d3b75bc0b87e3f2931d29771c38023722c926
mhbitarafan <43083274+mhbitarafan@users.noreply.github.com>
2024-11-24 17:20:38 +0330

    Update Sleep.hs

--- Sleep.hs
+++ Sleep.hs
@@ -1,8 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Network.HTTP.Conduit (simpleHttp)
 import Control.Concurrent (threadDelay)
 
 main :: IO ()
 main = do
+    putStrLn "Fetching http://example.com ..."
+    response <- simpleHttp "https://example.com"
+    putStrLn "Response received:"
+    putStrLn $ take 500 (show response) ++ "..." -- Print the first 500 characters of the response
     putStrLn "Sleeping for 5 seconds..."
     threadDelay (5 * 1000000) -- threadDelay takes microseconds
-    putStrLn "Done sleeping!"
- 
+    putStrLn "Done!"
+
