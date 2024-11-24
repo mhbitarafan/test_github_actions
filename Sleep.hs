c5629ded5308386186a2ca6bc6ba887551872c34
mhbitarafan <43083274+mhbitarafan@users.noreply.github.com>
2024-11-24 17:03:54 +0330

    Create Sleep.hs

---
+++ Sleep.hs
@@ -0,0 +1,7 @@
+import Control.Concurrent (threadDelay)
+
+main :: IO ()
+main = do
+    putStrLn "Sleeping for 5 seconds..."
+    threadDelay (5 * 1000000) -- threadDelay takes microseconds
+    putStrLn "Done sleeping!"
